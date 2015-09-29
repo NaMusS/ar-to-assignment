cd713721ff6a252bab2ffe2a61be167d72006687
nextacademy <nextacademy@email.com>
2015-09-29 20:34:55 +0800

    testing out list commands

--- todo.rb
+++ todo.rb
@@ -4,7 +4,36 @@ require 'byebug'
 #add a task
 #delete a task
 #mark a task as done/complete
-byebug
 
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+command = ARGV.shift
+string = ARGV.join(" ")
+
+case command
+when "list"
+	puts "Task List"
+	puts "========"
+	tasks = Task.all
+  tasks.each_with_index do |task, index|
+  	if task.complete == 0
+  	puts "#{index + 1}. #{task.name} [NOT DONE]"
+  	else
+  		puts "#{index + 1}. #{task.name} [DONE]"
+  	end
+  end
+when "new"
+	Task.create(name: string, complete: 0)
+	# byebug
+	puts "create new task: "
+when "complete"
+		puts "set task to complete"
+when "delete"
+	puts "delete task"
+else
+	puts "Invalid Command"
+	puts "Available Command:"
+	puts "list - list all tasks"
+	puts "new[task name] - create new task"
+	puts "complete [id] - set task to complete"
+	puts "delete [id] - delete the task"
+end
