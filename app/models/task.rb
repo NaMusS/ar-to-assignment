12d3af8af706cac4c0e7fd7f187b5a8a1ea6e40b
nextacademy <nextacademy@email.com>
2015-09-29 19:19:18 +0800

    linked model to database tasks table

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,24 +1,6 @@
-
-
 class Task < ActiveRecord::Base
-
-def self.list
-  	puts "Here's your todo list:"
-  	tasks = Task.all
-  end
-
-	def self.add(new_task)
-	Task.create(description: new_task)
-  end
-
-  def self.delete(task_id)
-  	chore = Task.all[task_id - 1]
-  	task.delete
-  	puts "Your task '#{chore.description}' has been deleted"
-  end
-
-
-
 end
+
+
 
 
