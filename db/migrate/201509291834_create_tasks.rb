bfbba6017660047c98710f8e9249ef9125b1f73a
nextacademy <nextacademy@email.com>
2015-09-29 18:37:30 +0800

    Setup database

--- db/migrate/201509291834_create_tasks.rb
+++ db/migrate/201509291834_create_tasks.rb
@@ -1,10 +1,9 @@
-require_relative '../../config/application'
 
 class CreateTasks < ActiveRecord::Migration
 	def change
 		create_table :tasks do |t|
-			t.string :description
-			t.string :completed
+			t.string :name
+			t.integer :complete
 		end
 	end
 end
